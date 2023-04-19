c91f61290acb2d72f4e67e66fc369060a2374f64
sahinmehmet3777 <sahinmehmet377@gmail.com>
2023-04-19 14:11:09 +0300

    Proje dosyası ekle.

---
+++ dapper/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace dapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.search = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dgvmusteri = new System.Windows.Forms.DataGridView();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btncancel = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnsearch = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvmusteri)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(69, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(34, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 26);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Soyadı:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(44, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 26);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Adres:";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.Red;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 410);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(841, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(127, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(147, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(147, 119);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(150, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(147, 169);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(150, 97);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(422, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 26);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Adı:";
+            // 
+            // search
+            // 
+            this.search.Location = new System.Drawing.Point(478, 72);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(121, 20);
+            this.search.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(356, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(127, 24);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Müşteri Listesi";
+            // 
+            // dgvmusteri
+            // 
+            this.dgvmusteri.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dgvmusteri.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvmusteri.Location = new System.Drawing.Point(360, 180);
+            this.dgvmusteri.Name = "dgvmusteri";
+            this.dgvmusteri.Size = new System.Drawing.Size(473, 219);
+            this.dgvmusteri.TabIndex = 4;
+            // 
+            // btnsave
+            // 
+            this.btnsave.FlatAppearance.BorderSize = 3;
+            this.btnsave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnsave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnsave.ForeColor = System.Drawing.Color.Red;
+            this.btnsave.Location = new System.Drawing.Point(37, 304);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(89, 37);
+            this.btnsave.TabIndex = 5;
+            this.btnsave.Text = "Kaydet";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btncancel
+            // 
+            this.btncancel.FlatAppearance.BorderSize = 3;
+            this.btncancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btncancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btncancel.ForeColor = System.Drawing.Color.Red;
+            this.btncancel.Location = new System.Drawing.Point(227, 304);
+            this.btncancel.Name = "btncancel";
+            this.btncancel.Size = new System.Drawing.Size(89, 37);
+            this.btncancel.TabIndex = 6;
+            this.btncancel.Text = "Vazgeç";
+            this.btncancel.UseVisualStyleBackColor = true;
+            // 
+            // btndelete
+            // 
+            this.btndelete.FlatAppearance.BorderSize = 3;
+            this.btndelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btndelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btndelete.ForeColor = System.Drawing.Color.Red;
+            this.btndelete.Location = new System.Drawing.Point(147, 304);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(52, 37);
+            this.btndelete.TabIndex = 7;
+            this.btndelete.Text = "Sil";
+            this.btndelete.UseVisualStyleBackColor = true;
+            // 
+            // btnsearch
+            // 
+            this.btnsearch.FlatAppearance.BorderSize = 3;
+            this.btnsearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnsearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnsearch.ForeColor = System.Drawing.Color.Red;
+            this.btnsearch.Location = new System.Drawing.Point(605, 63);
+            this.btnsearch.Name = "btnsearch";
+            this.btnsearch.Size = new System.Drawing.Size(80, 36);
+            this.btnsearch.TabIndex = 8;
+            this.btnsearch.Text = "Arama";
+            this.btnsearch.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(841, 432);
+            this.Controls.Add(this.btnsearch);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btncancel);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.dgvmusteri);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.search);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Müşteri Bilgileri";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvmusteri)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox search;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dgvmusteri;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btncancel;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnsearch;
+    }
+}
+
---
+++ dapper/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using dapper;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace dapper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //string connectionString = "Data Source=216-15\\SQLEXPRESS;Initial Catalog=Musteriler;User ID=sa; Password=Fbu123456";
+        //string connectionString1 = "Data Source = 216-15\\SQLEXPRESS;Initial Catalog = Musteriler; User ID = sa Password=Fbu123456";
+        
+        SqlConnection SqlCon = new SqlConnection("Data Source=216-15\\SQLEXPRESS;Initial Catalog=Musteriler;User ID=sa; Password=Fbu123456");
+
+        int id = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                FillDataGridView();
+                clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        void FillDataGridView()
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@Aramametni", search.Text.Trim());
+            List<MusteriBilgileri> list = SqlCon.Query<MusteriBilgileri>("MusteriArama", param, commandType: CommandType.StoredProcedure).ToList<MusteriBilgileri>();
+            dgvmusteri.DataSource = list;
+            dgvmusteri.Columns[0].Visible= false;
+
+        }
+        void clear()
+        {
+
+        }
+
+        class MusteriBilgileri
+        {
+            public int id { get; set; }
+            public string Adı { get; set; }
+            public string Soyadı { get; set; }
+            public bool sil { get; set; }
+            public string Adres { get; set; }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
